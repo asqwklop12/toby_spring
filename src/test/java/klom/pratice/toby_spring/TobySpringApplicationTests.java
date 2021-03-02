c0186a787cfcb6b1db47a7f446c108bc413fa026
asqwklop12 <asqwklop12@gmail.com>
2021-03-02 22:43:49 +0900

    Initial commit

---
+++ src/test/java/klom/pratice/toby_spring/TobySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package klom.pratice.toby_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
